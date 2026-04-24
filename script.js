a498ee8d3d23d6e654d5d5e0657a7b607b2a282e
birbalkr <birbalkr1435@gmail.com>
2026-04-24 22:40:05 +0530

    foreach map filter find indexOf

--- script.js
+++ script.js
@@ -3,3 +3,36 @@
 //functions return
 //async js coding
 
+var arr = [1, 12, 3, 14, 5];
+// foreach map filter find indexOf
+
+arr.forEach(function (val) {
+    val
+    console.log(val);
+    
+})
+
+var maparr=arr.map(function (val) {
+    return val;
+    
+})
+
+console.log(maparr);
+
+
+var filterarr = arr.filter(function (val) {
+    if (val>=12) {return true;}
+    else return false;
+})
+
+console.log(filterarr);
+
+var findarr = arr.find(function (val) {
+    if (val>=12) {return true;}
+    else return false;
+})
+
+console.log(findarr);
+
+var indexofarr = arr.indexOf(3);
+console.log(indexofarr);
