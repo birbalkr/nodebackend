dbbecc23343a41b43a27680dd3d5f16378179034
birbalkr <birbalkr1435@gmail.com>
2026-04-25 08:46:13 +0530

    object

--- script.js
+++ script.js
@@ -3,36 +3,55 @@
 //functions return
 //async js coding
 
-var arr = [1, 12, 3, 14, 5];
-// foreach map filter find indexOf
-
-arr.forEach(function (val) {
-    val
-    console.log(val);
+    var obj = {
+        name: "Birbal",
+        age: 22,
+        city: "delhi",
+    }
     
-})
+    Object.freeze(obj); // freeze the object to prevent any changes
 
-var maparr=arr.map(function (val) {
-    return val;
+
+    console.log(obj.name);
+
+     // update age of obj
+    obj.age= 23;
+
+    console.log(obj.age);
     
-})
-
-console.log(maparr);
+    
 
 
-var filterarr = arr.filter(function (val) {
-    if (val>=12) {return true;}
-    else return false;
-})
+// var arr = [1, 12, 3, 14, 5];
+// // foreach map filter find indexOf
 
-console.log(filterarr);
+// arr.forEach(function (val) {
+//     val
+//     console.log(val);
+    
+// })
 
-var findarr = arr.find(function (val) {
-    if (val>=12) {return true;}
-    else return false;
-})
+// var maparr=arr.map(function (val) {
+//     return val;
+    
+// })
 
-console.log(findarr);
+// console.log(maparr);
 
-var indexofarr = arr.indexOf(3);
-console.log(indexofarr);
+
+// var filterarr = arr.filter(function (val) {
+//     if (val>=12) {return true;}
+//     else return false;
+// })
+
+// console.log(filterarr);
+
+// var findarr = arr.find(function (val) {
+//     if (val>=12) {return true;}
+//     else return false;
+// })
+
+// console.log(findarr);
+
+// var indexofarr = arr.indexOf(3);
+// console.log(indexofarr);
